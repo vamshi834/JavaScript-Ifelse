827cee9abe8afe66dcd7169d8c3c25fcf25d5359
Vamshi Medipally <vamshi.m834@gmail.com>
2021-10-13 07:07:38 +0530

    added a js file

---
+++ main.js
@@ -0,0 +1,51 @@
+if (10>7){
+    console.log(true)
+}else{
+    console.log(false)
+  }
+
+if (10<7){
+    console.log(true)
+}
+  else{
+    console.log(false)
+  }
+if (!(10>7)){
+  console.log(true)
+}else{
+  console.log(false)
+}
+if (!(10<7)){
+  console.log(true)
+}else{
+  console.log(false)
+}
+var num = 10
+if (num>11){
+  console.log(true)
+}else{
+ console.log("not true") 
+}
+var num = 10
+if (!(num>11)){
+  console.log(true)
+}else{
+ console.log(false) 
+}
+var currentday = "tue"
+if (currentday === "mon") {
+  console.log("mon time")
+}else if (currentday === "tue") {
+  console.log("tue ton")
+}else if (currentday === "wed") {
+  console.log("wed tin")
+}
+var currentday = "wed"
+if (currentday === "mon") {
+  console.log("mon time")
+}else if (currentday === "tue") {
+  console.log("tue ton")
+}else if (currentday === "wed") {
+  console.log("wed tin")
+}
+
